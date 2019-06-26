875fe4915041d690f6bad647ba85bae4393d3e8c
dejavudwh <494319000@qq.com>
2019-06-26 20:24:54 +0800

    updated login and app router

--- src/component/App.js
+++ src/component/App.js
@@ -9,41 +9,12 @@ import AddressBook from './AddressBook.js';
 class App extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {
-        LeftSidebar: 'recent',
-        RightSidebar: 'share',
-      };
-      this.componentMap = {
-        'recent' : <SocialBar />,
-        'information' : <InfoTable /> ,
-        'address' : <AddressBook />,
-      };
-      this.changeLeftSidebar = this.changeLeftSidebar.bind(this);
-      this.changeRightSidebar = this.changeRightSidebar.bind(this);
-    }
-
-    changeLeftSidebar(event) {
-      const t = event.target.id === '' ? this.state.LeftSidebar : event.target.id
-      this.setState({
-        LeftSidebar: t,
-      })
-    }
-
-    changeRightSidebar(event) {
-      console.log(event.target.id)
-      const t = event.target.id === '' ? this.state.RightSidebar : event.target.id
-      this.setState({
-        RightSidebar: t,
-      })
     }
 
     render() {
       return (
         <div>
-          <LeftNav onChangeLeft={this.changeLeftSidebar} onChangeRight={this.changeRightSidebar}/>
-          {this.componentMap[this.state.LeftSidebar]}
-          <ChatBar />
-          <Sidebar species={this.state.RightSidebar}/>
+          {this.props.children}
         </div>
       )
     };
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './component/App.js';
 import Login from './component/Login.js';
+import MainPanel from './component/MainPanel.js';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 
 ReactDOM.render((
   <Router history={browserHistory}>
     <Route path="/" component={App}>
-      <Route path="home" component={Login} />
+      <IndexRoute component={Login} />
+      <Route path="/home" component={MainPanel} />
     </Route>
   </Router>
 ), document.getElementById('root'));
